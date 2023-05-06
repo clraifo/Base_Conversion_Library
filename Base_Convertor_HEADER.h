2adfbe598e42347264c815900e94865dcacab2f2
clraifo <41687507+clraifo@users.noreply.github.com>
2023-05-06 08:42:17 -0400

    Create Base_Convertor_HEADER.h

---
+++ Base_Convertor_HEADER.h
@@ -0,0 +1,10 @@
+#ifndef BASECONVERTER_H
+#define BASECONVERTER_H
+
+#include <string>
+
+namespace BaseConverter {
+    std::string ConvertBase(const std::string& number, int baseFrom, int baseTo);
+}
+
+#endif
