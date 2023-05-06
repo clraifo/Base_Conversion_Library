8f207d99a3f165d845bdf37711f702f94dfc1990
clraifo <41687507+clraifo@users.noreply.github.com>
2023-05-06 08:56:33 -0400

    Update Base_Convertor_HEADER.h
    Added lots of comments

--- Base_Convertor_HEADER.h
+++ Base_Convertor_HEADER.h
@@ -1,10 +1,15 @@
-#ifndef BASECONVERTER_H
-#define BASECONVERTER_H
+#ifndef BASECONVERTER_H              // This is a preprocessor directive that checks if the symbol "BASECONVERTER_H" has not been defined
+#define BASECONVERTER_H              // If "BASECONVERTER_H" has not been defined, it is defined in this line
 
-#include <string>
+#include <string>                    // This includes the string header file, which provides string-related functions and operations
 
-namespace BaseConverter {
+namespace BaseConverter {            // This creates a new namespace named "BaseConverter"
     std::string ConvertBase(const std::string& number, int baseFrom, int baseTo);
+    // This declares a function named "ConvertBase" that takes three parameters:
+    //   - A const reference to a string named "number", which represents the number to be converted
+    //   - An integer named "baseFrom", which represents the base system of the input number
+    //   - An integer named "baseTo", which represents the base system to convert the number to
+    // The function returns a string, which is the converted number in the new base system
 }
 
-#endif
+#endif                               // This marks the end of the preprocessor directive
